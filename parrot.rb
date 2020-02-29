0582f41396a1a3abccabe01fa878d1b41c3ea210
Tariq M Sulaiman <tariqsulaiman11@gmail.com>
2020-02-29 02:38:07 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,14 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  talk = "Squawk!"
-  puts "Hello #{talk}"
-  return "Hello #{talk}"
-end
 
-parrot(Tariq)
-
-def parrot(say="Squawk!")
-  puts "#{string}"
-  "#{string}"
+def parrot(say = "Squawk!")
+  puts "#{say}"
+  "#{say}"
 end
